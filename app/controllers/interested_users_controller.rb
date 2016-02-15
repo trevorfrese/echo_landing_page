59ad9c49012e8499153d737602a28f7cf869a80c
Trevor Frese <trevor.frese@gmail.com>
2016-02-15 13:09:09 -0800

    Add favicon, rework submit logic, add content to page

--- app/controllers/interested_users_controller.rb
+++ app/controllers/interested_users_controller.rb
@@ -1,8 +1,13 @@
 class InterestedUsersController < ApplicationController
   def sign_up
     @interested_user = InterestedUser.new
-    @interested_user.update_attributes!(interested_user_params)
-    flash['success']= "Thanks for signing up! We will send you more information as we grow"
+    params = interested_user_params
+    if User.where(email: params['email'])
+      flash['notice']= "The email you entered is already on our testers list."
+    else
+      @interested_user.update_attributes!(params)
+      flash['success']= "Thanks for signing up! We will send you more information as we grow"
+    end
     redirect_to '/'
   rescue ActiveRecord::RecordInvalid
     flash['error']= "Invalid email."

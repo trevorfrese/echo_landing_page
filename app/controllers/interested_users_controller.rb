f11d7de613f494051541a97dade3fb3f28c05e67
Trevor Frese <trevor.frese@gmail.com>
2016-02-16 10:40:52 -0800

    Fixed the email logic

--- app/controllers/interested_users_controller.rb
+++ app/controllers/interested_users_controller.rb
@@ -2,11 +2,11 @@ class InterestedUsersController < ApplicationController
   def sign_up
     @interested_user = InterestedUser.new
     params = interested_user_params
-    if User.where(email: params['email'])
-      flash['notice']= "The email you entered is already on our testers list."
-    else
+    if InterestedUser.where(email: params['email']).blank?
       @interested_user.update_attributes!(params)
       flash['success']= "Thanks for signing up! We will send you more information as we grow"
+    else
+      flash['notice']= "The email you entered is already on our testers list."
     end
     redirect_to '/'
   rescue ActiveRecord::RecordInvalid
